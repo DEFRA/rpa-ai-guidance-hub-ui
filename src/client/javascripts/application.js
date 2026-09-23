d86c9456ba88a59eb1e28c005cdc51f38d8a14b3
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2026-09-23 16:46:55 +0100

    Remove redundant tab query string

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -1,8 +1,7 @@
 import * as govukFrontend from 'govuk-frontend'
-import { initHubTabs } from './hub-tabs.js'
+
 import { initPolling } from './status-poll.js'
 
 govukFrontend.initAll()
 
 initPolling()
-initHubTabs()
